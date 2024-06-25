a05333d90b7ca3ecda22693cfa8d88ac7db09a0f
Orhan Erday <orhan.erday@orioninc.com>
2024-06-25 13:39:35 +0300

    Final touch

--- combine-text-files.rb
+++ combine-text-files.rb
@@ -2,7 +2,7 @@ class CombineTextFiles < Formula
     desc "Combine text files in a directory into a single file"
     homepage "https://github.com/orhanerday/combine-text-files"
     url "https://github.com/orhanerday/combine-text-files/archive/refs/tags/v1.0.tar.gz"
-    sha256 "your_generated_sha256_checksum"
+    sha256 "1f76898e1b2beff6404b4106a00277e54e4cde85c4d60c9d006e8579e3d98c45"
     license "MIT"
   
     depends_on "python@3.9"
